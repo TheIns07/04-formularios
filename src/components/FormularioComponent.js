fbf984290de21fd4a97c8252c8577979ece5fbee
Arturo Cervantes <jesus.cervantes207003@potros.itson.edu.mx>
2022-06-29 19:31:01 -0600

    Fomularios completados

--- src/components/FormularioComponent.js
+++ src/components/FormularioComponent.js
@@ -10,15 +10,28 @@ export const FormularioComponent = () => {
       nombre: datos.nombre.value,
       apellido: datos.apellido.value,
       genero: datos.genero.value,
-      bio: datos.bio.value
+      bio: datos.bio.value,
+      enviar: datos.enviar.value
     }
     setUsuario(usuario);
+  }
+  const cambiarDatos = e => {
+    let input_name = (e.target.name);
+    let user_modify = usuario;
+
+    //user_modify[input_name] = e.target.value;
+    setUsuario(previous_state => ({
+
+        ...previous_state,
+        [input_name]: e.target.value
+      
+    }));
   }
 
   return (
     <div>
         <h1>Formularios con react</h1>
-        {usuario.bio && usuario.bio.length >= 1 && 
+        {usuario.enviar&& 
           (<div className='infoUser'>
           El nombre es {usuario.nombre} {usuario.apellido} y su genero es {usuario.genero}
           y su biografia es {usuario.bio}
@@ -29,22 +42,25 @@ export const FormularioComponent = () => {
           <div className='inputs'>
           <input type = "text" 
           placeholder='Nombre'
-          name = "nombre"/>
+          name = "nombre"
+          onChange= {cambiarDatos}
+          />
   
           <input 
           type = "text" 
           placeholder='Apellido'
           name = "apellido"
+          onChange= {cambiarDatos}
           />
           </div>
           
-          <select name ="genero">
+          <select name ="genero" onChange= {cambiarDatos}>
             <option value ="hombre">Hombre</option>
             <option value ="mujer">Mujer</option>
           </select>
-          <textarea placeholder='Biografia' name ="bio"></textarea>
+          <textarea placeholder='Biografia' onChange= {cambiarDatos} name ="bio"></textarea>
 
-          <input type = "submit" value = "Enviar"/>
+          <input type = "submit" value = "Enviar" name = "enviar"/>
         </form>
     </div>
   )
